a3d0677c8409fe536006c4f15de615fab193f436
Jose Paredes <joseaplwork@gmail.com>
2024-05-21 12:10:10 +0200

    refactor: update empty-view and error-view components to remove unnecessary code

--- src/pages/home/components/empty-view.component.tsx
+++ src/pages/home/components/empty-view.component.tsx
@@ -1,6 +1,6 @@
 export function EmptyView() {
   return (
-    <div className="flex justify-center items-center flex-col pt-20">
+    <div className="flex items-center flex-col pt-20">
       <img width={200} src="/empty-box.png" alt="empty" />
       <b className="mt-5 text-slate-600">No results</b>
     </div>
--- src/shared/components/error-view.component.tsx
+++ src/shared/components/error-view.component.tsx
@@ -1,6 +1,6 @@
 export function ErrorView() {
   return (
-    <div className="flex justify-center items-center flex-col pt-20">
+    <div className="flex items-center flex-col pt-20">
       <img width={200} src="/alarm-sign.png" alt="error" />
       <b className="mt-5 text-slate-600">Something went wrong</b>
     </div>
